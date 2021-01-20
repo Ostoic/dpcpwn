b9b1ca7f68ec4af4361f9a083f6d36e77b8f6152
ostoic <dinnertable@hotmail.ca>
2021-01-19 20:32:40 -0500

    Added setup script

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='dpcpwn',
+    version='v1.0',
+    packages=[''],
+    url='https://github.com/Ostoic/dpcpwn',
+    license='GPL',
+    author='fure',
+    author_email='dinnertable@hotmail.ca',
+    description='DPC3848VM RCE PoC'
+)
